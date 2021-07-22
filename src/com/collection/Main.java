0d7547d81aaae5a51788b4fa730c35f3a27e321f
Alex <neperamojny@gmail.com>
2021-07-21 18:52:31 -0700

    Third commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -6,6 +6,7 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello World");
             System.out.println("Tuna is yummy");
+            System.out.println("Salmon is yummy too");
             Thread.sleep(2000);
         }
     }
